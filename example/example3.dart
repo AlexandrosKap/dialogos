53e968a48fd3d4a19b10b44a2808e61cff3800a9
AlexandrosKap <alexandroskapretsos@gmail.com>
2022-06-22 01:06:13 +0300

    added example text game

---
+++ example/example3.dart
@@ -0,0 +1,55 @@
+import 'dart:io' show stdin, sleep;
+
+import 'package:dialogos/dialogos.dart';
+
+String showMenu(Line event) {
+  final positions = event.menuEventPositions;
+  final options = event.menuEventOptions;
+
+  // Print options.
+  print('');
+  var index = 0;
+  for (var option in options) {
+    print('$index => $option');
+    index++;
+  }
+
+  // Returns a position from the options.
+  print('Enter a option:');
+  while(true) {
+    final number = int.tryParse(stdin.readLineSync() ?? '');
+    if (number != null && number >= 0 && number < options.length) {
+      print('');
+      return positions[number];
+    }
+  }
+}
+
+void printLine(Line line) {
+  if (line.name.isEmpty) {
+    print(line);
+  } else {
+    print('${line.name}: $line');
+  }
+}
+
+void main() async {
+  const scene = 'level2/questionGame';
+
+  final lineManager = LineManager();
+  await lineManager.load('example/assets/lines/en.csv');
+  final dialogue = Dialogue(lineManager);
+
+  dialogue.start(scene);
+  while (dialogue.hasNext) {
+    final line = dialogue.line;
+    print('');
+    if (line.isMenuEvent) {
+      printLine(dialogue.gotoPosition(showMenu(line)));
+    } else {
+      printLine(line);
+    }
+    sleep(Duration(seconds: 1));
+    dialogue.next();
+  }
+}
